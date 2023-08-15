3617cb7abd298a104d5d665d863cf7ec21b67db0
alwanes <alwanes@DESKTOP-L38MR74>
2023-08-15 15:03:36 +0300

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter Your Number:");
+            int num = int.Parse(Console.ReadLine());
+            Console.Clear();
+            for(int i = 0; i <=12; i++)
+            {
+                int R = i * num;
+                Console.WriteLine(num+"*"+i+"="+R);          }
+        }
+    }
+}
